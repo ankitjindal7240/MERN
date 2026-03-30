e2e25ea1333bb6a6ae6075c7379a2e9fe95351ca
Ankit <ankitjindal8787@gmail.com>
2026-03-30 19:58:14 +0530

    ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,6 +4,9 @@ import {connectDB} from './config/db.js'
 import dotenv from 'dotenv'
 import rateLimiter from './middleware/ratelimiter.js'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from "url";
+
 dotenv.config()
 
 const app = express()
@@ -12,6 +15,17 @@ app.use(cors())
 app.use(rateLimiter)
 app.use('/api/notes',notesRoutes)
 const PORT = process.env.PORT || 5001
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+if (process.env.NODE_ENV === "production") {
+  const distPath = path.join(__dirname, "../../Frontend/thinkboard/dist");
+
+  app.use(express.static(distPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+};
 
 connectDB().then(() =>{
 app.listen(PORT,()=>{
